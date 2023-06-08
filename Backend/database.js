4d5e2a360984adb4adbe9dd27ae82416d7d7eec7
AizenSousuke <kurono1807@gmail.com>
2023-06-08 08:06:39 -0500

    fixxx

--- Backend/database.js
+++ Backend/database.js
@@ -2,10 +2,15 @@ const mongoose = require("mongoose")
 const dotenv = require('dotenv')
 dotenv.config();
 
-mongoose.connect(process.env.MONGODB_URI, {
+async function connectToMongoDB () {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-}).then(() => {
+  })
   console.log("Conexión a MongoDB establecida en puerto");
-}).catch(err => console.log('Error de conexión a MongoDB:', err));
+ } catch (error) {
+  console.log('Error de conexión a MongoDB:', err)};
+ }
 
+ connectToMongoDB()
