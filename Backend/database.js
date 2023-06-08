87197d5f1f88d2f9062bcc966a23268b64e42981
AizenSousuke <kurono1807@gmail.com>
2023-06-07 22:37:24 -0500

    fix PORT

--- Backend/database.js
+++ Backend/database.js
@@ -1,11 +1,12 @@
 const mongoose = require("mongoose")
-const dotenv = require('dotenv');
+const PORT = process.env.PORT || 5000
+const dotenv = require('dotenv')
 dotenv.config();
 
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }).then(() => {
-  console.log('Conexión a MongoDB establecida');
+  console.log(`Conexión a MongoDB establecida en puerto ${PORT}`);
 }).catch(err => console.log('Error de conexión a MongoDB:', err));
 
