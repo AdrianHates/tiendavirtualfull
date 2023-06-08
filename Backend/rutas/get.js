fe7937dc26691e0a1cafad75c3f858b5602fb7b5
AizenSousuke <kurono1807@gmail.com>
2023-06-08 10:43:22 -0500

    xxx

--- Backend/rutas/get.js
+++ Backend/rutas/get.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const router = express.Router();
 const Product = require('../esquemas/products'); // Importa el modelo de productos
+const router = express.Router();
 const app = express();
 // todos los productos
 router.get('/products', async (req, res) => {
   try {
-    const productos = await Product.find().maxTimeMS(15000); 
+    const productos = await Product.find(); 
     res.status(200).json(productos); 
   } catch (error) {
     res.status(500).json({ message: "el error viene de aqui?"}); 
